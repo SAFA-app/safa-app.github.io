de3e73884aa2196d1471c9ba91a2ac60e375499b
SAFA-app <lpausini059@gmail.com>
2025-03-21 09:43:45 +0000

    Custom page logic

--- js/items/main.js
+++ js/items/main.js
@@ -3,9 +3,9 @@ import { getValidPages } from '/js/page-utils.js'; // Import utility function to
 document.addEventListener('DOMContentLoaded', async () => {
     const urlParams = new URLSearchParams(window.location.search);
     const parentId = urlParams.get('parent'); // Retrieve the parent URL parameter
-    
+
     const validPages = await getValidPages();
-    
+
     // Filter pages based on the parentId or show rows with empty parent field
     const filteredPages = validPages.filter(page => {
         if (parentId) {
@@ -16,8 +16,8 @@ document.addEventListener('DOMContentLoaded', async () => {
             return page.parent === "";
         }
     });
-    
-    
+
+
     // Populate the list with filtered pages
     const itemsListContainer = document.getElementById('items-list-container');
     itemsListContainer.innerHTML = ''; // Clear any existing content
@@ -57,15 +57,22 @@ function createListItem(page, validPages) {
             // If there are child items, reload the same page with 'parent' set to this item's 'id'
             window.location.href = `items.html?parent=${page.id}`;
         } else {
-            // If the page has no children and the 'external_link' field is not empty, redirect to the external link
-            if (page.external_link) {
-                window.location.href = page.external_link;
+            // If the page has no children and the 'custom_page' field is "TRUE"
+            if (page.custom_page === "TRUE") {
+                // Redirect to the custom page based on 'id'
+                window.location.href = `/pages/custom/${page.id}.html`;
             } else {
-                // Otherwise, redirect to the 'single-item.html' page with 'id' as a URL parameter
-                window.location.href = `single-item.html?id=${page.id}`;
+                // If the 'custom_page' is not "TRUE", open the 'single-item.html' page with 'id' as a URL parameter
+                if (page.external_link) {
+                    window.location.href = page.external_link;
+                } else {
+                    // Otherwise, redirect to the 'single-item.html' page with 'id' as a URL parameter
+                    window.location.href = `single-item.html?id=${page.id}`;
+                }
             }
         }
     });
+
 
     return itemDiv;
 }
