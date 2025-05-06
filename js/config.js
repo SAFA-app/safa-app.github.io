b36a60b781b93def7c52ded5747bab964818fe05
SAFA-app <lpausini059@gmail.com>
2025-05-06 13:32:20 +0000

    Compare form id sha1 hashes with the ones in config

--- js/config.js
+++ js/config.js
@@ -14,6 +14,8 @@ const config = {
     pagesTable: "https://docs.google.com/spreadsheets/d/1ZuUVyBmIU_Ax5V7Iq-yCUQvN-tmF8RZEh1uhkT6HVFA/export?format=csv",
     deletedTable: "https://docs.google.com/spreadsheets/d/1FVN90zGMNJbKOiBJWCVdVH7UFI74yny4G-3vJBzwrEo/export?format=csv",
     deletionEntryId: 'entry.1372900842',
+    creationFormIdHsh: '822cefd8fbee2e592919a29708bab929a84d1ce0', //SHA1 Hash
+    deletionFormIdHash: 'fab33b18a44810eae7bba397c049b7de514894a5' //SHA1 Hash
 };
 
 export default config;
